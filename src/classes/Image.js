1a4d87c7d6e7d22e0f50d804ddb09733e57cca48
Dooley <nsewolo@gmail.com>
2015-02-17 23:08:57 -0500

    Static function added

--- src/classes/Image.js
+++ src/classes/Image.js
@@ -1,7 +1,14 @@
-export default
 class Image extends Picture {
 
     constructor(path, direction, brightness) {
         super(path, direction, brightness);
     }
+
+    static manipulate() {
+        return 'was manipulated by static function';
+    }
 }
+
+Image.STATIC_FIELD = "The static field";
+
+export default Image;
